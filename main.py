54cbbae1123ce337f1393545c26cca361a3bbc80
davidchung29 <60008214+dvjr2018@users.noreply.github.com>
2021-02-25 08:50:23 -0800

    created solve_maze_helper
    solve_maze_helper runs through a set of conditionals to find the solution using recursion and backtracking

--- main.py
+++ main.py
@@ -12,3 +12,41 @@ def print_maze(maze):
         print(row_print)
 
 
+
+
+def solve_maze_helper(maze, sol, pos_row, pos_col):
+    # size of maze
+    num_row = len(maze)
+    num_col = len(maze[0])
+
+    # base cases
+
+    # robot is home
+
+    if pos_row == num_row - 1 and pos_col == num_col - 1:
+        return sol
+
+    # out of bounds
+    if pos_row >= num_row or pos_col >= num_col:
+        return None
+
+    # is on an obstacle
+    if maze[pos_row][pos_col] == 'X':
+        return None
+
+    # try right
+    sol.append("r")
+    sol_going_right = solve_maze_helper(maze, sol, pos_row, pos_col + 1)
+    if sol_going_right is not None:
+        return sol_going_right
+
+    # backtrack
+    sol.pop()
+    sol.append("d")
+    sol_going_down = solve_maze_helper(maze, sol, pos_row + 1, pos_col)
+    if sol_going_down is not None:
+        return sol_going_down
+
+    # no solution
+    sol.pop()
+    return None
